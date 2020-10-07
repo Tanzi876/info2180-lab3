ec7fbafc513792b98c1a29602cd4c209a310728e
Tanzi876 <tanyaleelawrence@gmail.com>
2020-10-07 12:57:16 -0500

    Added

---
+++ tic-tac-toe.js
@@ -0,0 +1,12 @@
+<script>
+
+document.querySelector("div")
+.classList.add("status");
+
+document.querySelector("div")
+.classList.add("board");
+
+document.getElementById("board").onclick
+=myFunction();
+
+</script>
