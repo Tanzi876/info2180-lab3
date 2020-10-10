0fc7f34f6adc37b2bf0ee721cbc2b415cb3ff5b4
Tanzi876 <tanyaleelawrence@gmail.com>
2020-10-10 15:48:02 -0500

    Added

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,19 +1,11 @@
 let xturn
-let state_X=[]
 let state_O=[]
-const WINNING_COMBINATIONS = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-  ]
+let state_X=[]
+
 window.onload=function (){
     let board=document.querySelectorAll('#board div')
-    let button=document.getElementsByClassName('btn')
+    let button=document.getElementById('button')
+    var XO
     xturn= false
         
     for (var i=0;i<board.length;i++){
@@ -22,14 +14,14 @@ window.onload=function (){
            console.log("box click")
             if (xturn==true){
                 board[i].innerHTML="X"
-                board[i].classList.add('square X')
+                board[i].classList.add('square.X')
                 console.log("Play X")
                 state_X.push("X")
                 swap()
             }else
             {
                 board[i].innerHTML="O"
-                board[i].classList.add('square o')
+                board[i].classList.add('square.O')
                 console.log("Play O")
                 state_O.push("O")
                 swap()
@@ -67,23 +59,23 @@ function Checkwin(){
     var status=document.querySelector('#status div')
     WINNING_COMBINATIONS.some(combo => {
         combo.every(index => {
-        if (state_X.length>state_O.length && 
-            board[index]=='X' ){
-            console.log("X wins")
-            status.classList.add('you-won')
-            status.innerHTML="Congratulations! X is the Winner!"
-        }else if (state_O.length>state_X.length && 
-            board[index]=='O'){
-                console.log("O wins")
-            status.classList.add('you-won')
-            status.innerHTML="Congratulations! O is the Winner!"
-        }else{
-            status.innerHTML="Game Draw"
-            console.log("Game Draw")
+            if (state_X.length>state_O.length && 
+                state_X[index]=='X' ){
+                console.log("X wins")
+                status.classList.add('you-won')
+                status.innerHTML="Congratulations! X is the Winner!"
+            }else if (state_O.length>state_X.length && 
+                state_O[index]=='O'){
+                    console.log("O wins")
+                status.classList.add('you-won')
+                status.innerHTML="Congratulations! O is the Winner!"
+            }else{
+                status.innerHTML="Game Draw"
+                console.log("Game Draw")
+    
+            }
+        })
+    })
 
-        }
 
-
-    }))
-
-    }
+}
